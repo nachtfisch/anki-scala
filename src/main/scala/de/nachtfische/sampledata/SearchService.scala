7601e843c5321d00576b5f1d24f7bc2dfa8b19c0
Christian <ch.fish@gmail.com>
2015-08-21 11:00:26 +0200

    added sample for elastic search

---
+++ src/main/scala/de/nachtfische/sampledata/SearchService.scala
@@ -0,0 +1,39 @@
+package de.nachtfische.sampledata
+
+import de.nachtfische.CommonConst
+import de.nachtfische.ankimodel.FlashCard
+import org.elasticsearch.action.index.IndexRequestBuilder
+import org.elasticsearch.common.settings.{ImmutableSettings, Settings}
+import org.elasticsearch.node.{NodeBuilder, Node}
+
+
+object SearchService {
+
+    val settings:Settings = ImmutableSettings
+      .settingsBuilder()
+      .put("node.http.enabled", true)
+      .put("path.data", CommonConst.PROJECT_PATH + "target/data")
+      .put("path.logs", CommonConst.PROJECT_PATH + "target/data")
+      .put("index.gateway.type", "none")
+      .put("index.store.type", "memory")
+      .put("index.number_of_shards", 1)
+      .put("index.number_of_replicas", 0)
+
+      .build();
+    val node:Node = NodeBuilder.nodeBuilder().settings(settings).node();
+    val client = node.client()
+
+
+
+    def search(query:String): List[String] = {
+        val builder: IndexRequestBuilder = new IndexRequestBuilder(client)
+        .setId("some")
+
+        List("some", "someOther")
+    }
+
+    def index(flashCard:FlashCard) = {
+
+    }
+
+}
