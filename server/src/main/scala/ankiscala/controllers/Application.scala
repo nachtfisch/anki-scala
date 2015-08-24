6d793ee2108b94e2c9bcc6a1432c9fe7d8744768
Christian <ch.fish@gmail.com>
2015-08-24 19:48:18 +0200

    changed server to play

---
+++ server/src/main/scala/ankiscala/controllers/Application.scala
@@ -0,0 +1,47 @@
+package ankiscala.controllers
+
+import java.nio.ByteBuffer
+
+import ankiscala.services.{API, ApiService}
+import boopickle.Default._
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Router extends autowire.Server[ByteBuffer, Pickler, Pickler] {
+  override def read[R: Pickler](p: ByteBuffer) = Unpickle[R].fromBytes(p)
+  override def write[R: Pickler](r: R) = Pickle.intoBytes(r)
+}
+
+object Application extends Controller {
+  val apiService = new ApiService()
+
+  def index = Action {
+    Ok(views.html.index("ankiscala"))
+  }
+
+  def autowireApi(path: String) = Action.async(parse.raw) {
+    implicit request =>
+      println(s"Request path: $path")
+
+      // get the request body as Array[Byte]
+      val b = request.body.asBytes(parse.UNLIMITED).get
+
+      // call Autowire route
+      Router.route[API](apiService)(
+        autowire.Core.Request(path.split("/"), Unpickle[Map[String, ByteBuffer]].fromBytes(ByteBuffer.wrap(b)))
+      ).map(buffer => {
+        val data = Array.ofDim[Byte](buffer.remaining())
+        buffer.get(data)
+        Ok(data)
+      })
+  }
+
+  def logging = Action(parse.anyContent) {
+    implicit request =>
+      request.body.asJson.foreach { msg =>
+        println(s"CLIENT - $msg")
+      }
+      Ok("")
+  }
+}
--- server/src/main/scala/de/nachtfische/web/RouteUtils.scala
+++
@@ -1,9 +0,0 @@
-package de.nachtfische.web
-import spray.http.MediaTypes
-import spray.routing.directives.{RespondWithDirectives, RouteDirectives}
-
-trait RouteUtils {
-    this: RespondWithDirectives with RouteDirectives =>
-
-    val completeWithJson = respondWithMediaType(MediaTypes.`application/json`) & complete
-}
